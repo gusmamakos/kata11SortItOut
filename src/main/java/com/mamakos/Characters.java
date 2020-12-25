2d1bf8246f5d0ea0de923eb8a130b2f2c0a79f0e
Constantine Mamakos <mamakos@andrew.cmu.edu>
2020-12-25 12:59:35 -0500

    cleaned up unused import statements and obvious comments

--- src/main/java/com/mamakos/Characters.java
+++ src/main/java/com/mamakos/Characters.java
@@ -1,21 +1,16 @@
 package com.mamakos;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class Characters {
     public String message;
 
     public Characters(String line) {
-        String temp = line.toLowerCase(); //lower case
+        String temp = line.toLowerCase();
 
-        Pattern pattern = Pattern.compile("[a-z]");//get characters only
+        Pattern pattern = Pattern.compile("[a-z]");
         Matcher matcher = pattern.matcher(temp);
         StringBuilder charactersOnly = new StringBuilder();
         while (matcher.find()) {
